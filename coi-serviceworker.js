a44bd9966240f743a57f0d4ac4a5186f05cacbe0
minimodproducts <davidminiatures@gmail.com>
2026-01-27 15:13:22 -0500

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,34 +1,6 @@
-/*! coi-serviceworker v0.1.7 | MIT License | https://github.com/gzuidhof/coi-serviceworker */
-if (typeof window === "undefined") {
-    self.addEventListener("install", () => self.skipWaiting());
-    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-
-    self.addEventListener("fetch", (event) => {
-        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
-            return;
-        }
-
-        event.respondWith(
-            fetch(event.request).then((response) => {
-                if (response.status === 0) {
-                    return response;
-                }
-
-                const newHeaders = new Headers(response.headers);
-                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
-                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-
-                return new Response(response.body, {
-                    status: response.status,
-                    statusText: response.statusText,
-                    headers: newHeaders,
-                });
-            })
-        );
-    });
-} else {
-    const n = navigator;
-    if (n.serviceWorker && n.serviceWorker.register) {
-        n.serviceWorker.register(window.document.currentScript.src);
-    }
-}
+/*! coi-serviceworker v0.1.6 - Guido Zuidhof, licensed under MIT */
+let co=self;co.addEventListener("install",()=>co.skipWaiting());co.addEventListener("activate",e=>e.waitUntil(co.clients.claim()));
+function handle(t,e,n){e.set("Cross-Origin-Embedder-Policy","require-corp");e.set("Cross-Origin-Opener-Policy","same-origin");}
+co.addEventListener("fetch",function(t){const e=t.request;if("only-if-cached"===e.cache&&"same-origin"!==e.mode)return;
+t.respondWith(fetch(e).then(t=>{if(0===t.status)return t;const n=new Headers(t.headers);handle(0,n,0);
+return new Response(t.body,{status:t.status,statusText:t.statusText,headers:n})}).catch(t=>console.error(t)))});
