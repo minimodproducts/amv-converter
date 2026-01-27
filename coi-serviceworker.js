b0694997a104c6d2d3b6cb53ebf840fd4475a763
minimodproducts <davidminiatures@gmail.com>
2026-01-27 16:00:49 -0500

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,6 +1,39 @@
-/*! coi-serviceworker v0.1.6 - Guido Zuidhof, licensed under MIT */
-let co=self;co.addEventListener("install",()=>co.skipWaiting());co.addEventListener("activate",e=>e.waitUntil(co.clients.claim()));
-function handle(t,e,n){e.set("Cross-Origin-Embedder-Policy","require-corp");e.set("Cross-Origin-Opener-Policy","same-origin");}
-co.addEventListener("fetch",function(t){const e=t.request;if("only-if-cached"===e.cache&&"same-origin"!==e.mode)return;
-t.respondWith(fetch(e).then(t=>{if(0===t.status)return t;const n=new Headers(t.headers);handle(0,n,0);
-return new Response(t.body,{status:t.status,statusText:t.statusText,headers:n})}).catch(t=>console.error(t)))});
+/*! coi-serviceworker v0.1.7 - Guido Zuidhof, licensed under MIT */
+let coepCredentialless = false;
+if (typeof window === 'undefined') {
+  self.addEventListener("install", () => self.skipWaiting());
+  self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+  self.addEventListener("fetch", function (event) {
+    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
+
+    let newHeaders = new Headers(event.request.headers);
+    if (newHeaders.get("range")) {
+        newHeaders.set("pos", newHeaders.get("range"));
+        newHeaders.set("range", "bytes=0-");
+    }
+
+    event.respondWith(
+      fetch(event.request, { htmlAcceptHeaders: ["text/html", "application/xhtml+xml"], headers: newHeaders })
+        .then((r) => {
+          const { status, statusText, body } = r;
+          if (status === 0 || status > 399) return r;
+
+          const headers = new Headers(r.headers);
+          headers.set("Cross-Origin-Embedder-Policy", coepCredentialless ? "credentialless" : "require-corp");
+          headers.set("Cross-Origin-Opener-Policy", "same-origin");
+
+          return new Response(body, { status, statusText, headers });
+        })
+    );
+  });
+} else {
+  (async () => {
+    if (window.crossOriginIsolated) return;
+    const registration = await navigator.serviceWorker.register(window.document.currentScript.src).catch((e) => console.error("COI: ", e));
+    if (registration) {
+        console.log("Reloading page to enable Cross-Origin Isolation");
+        window.location.reload();
+    }
+  })();
+}
