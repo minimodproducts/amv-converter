a55f5376f0eb798c16e51421a53f885d269d9340
minimodproducts <davidminiatures@gmail.com>
2026-01-27 20:48:17 -0500

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,38 +1,24 @@
-/*! coi-serviceworker v0.1.7 */
 if (typeof window === "undefined") {
     self.addEventListener("install", () => self.skipWaiting());
     self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
     self.addEventListener("fetch", (event) => {
-        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
-            return;
-        }
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
         event.respondWith(
             fetch(event.request).then((response) => {
                 if (response.status === 0) return response;
                 const newHeaders = new Headers(response.headers);
                 newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
                 newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-                return new Response(response.body, {
-                    status: response.status,
-                    statusText: response.statusText,
-                    headers: newHeaders,
-                });
+                return new Response(response.body, { status: response.status, statusText: response.statusText, headers: newHeaders });
             })
         );
     });
 } else {
-    (() => {
-        const script = document.currentScript;
-        const reload = () => { window.location.reload(); };
-        if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register(script.src).then((registration) => {
-                registration.addEventListener("updatefound", () => {
-                    if (navigator.serviceWorker.controller) reload();
-                });
-                if (navigator.serviceWorker.controller && !window.crossOriginIsolated) {
-                    reload();
-                }
-            });
-        }
-    })();
+    const script = document.currentScript;
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register(script.src).then((reg) => {
+            if (navigator.serviceWorker.controller) return;
+            window.location.reload();
+        });
+    }
 }
