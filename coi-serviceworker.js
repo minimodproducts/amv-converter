966cc63af8795a4c43bb89f96417a69377ce11b8
minimodproducts <davidminiatures@gmail.com>
2026-01-28 15:57:31 -0500

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,24 +1,82 @@
-if (typeof window === "undefined") {
-    self.addEventListener("install", () => self.skipWaiting());
-    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-    self.addEventListener("fetch", (event) => {
-        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
-        event.respondWith(
-            fetch(event.request).then((response) => {
-                if (response.status === 0) return response;
-                const newHeaders = new Headers(response.headers);
-                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
-                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-                return new Response(response.body, { status: response.status, statusText: response.statusText, headers: newHeaders });
-            })
-        );
-    });
-} else {
-    const script = document.currentScript;
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register(script.src).then((reg) => {
-            if (navigator.serviceWorker.controller) return;
-            window.location.reload();
-        });
+/*! coi-serviceworker v0.1.7 - Guido Zuidhof, licensed under MIT */
+let coepCredentialless = false;
+if (typeof window === 'undefined') {
+  self.addEventListener("install", () => self.skipWaiting());
+  self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+  self.addEventListener("message", (ev) => {
+    if (!ev.data) {
+      return;
+    } else if (ev.data.type === "deregister") {
+      self.registration.unregister();
+    } else if (ev.data.type === "coepCredentialless") {
+      coepCredentialless = ev.data.value;
     }
+  });
+
+  self.addEventListener("fetch", function (event) {
+    const r = event.request;
+    if (r.cache === "only-if-cached" && r.mode !== "same-origin") {
+      return;
+    }
+
+    const request = (coepCredentialless && r.mode === "no-cors")
+      ? new Request(r, {
+        credentials: "omit",
+      })
+      : r;
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          if (response.status === 0) {
+            return response;
+          }
+
+          const newHeaders = new Headers(response.headers);
+          newHeaders.set("Cross-Origin-Embedder-Policy",
+            coepCredentialless ? "credentialless" : "require-corp"
+          );
+          if (!coepCredentialless) {
+            newHeaders.set("Cross-Origin-Resource-Policy", "cross-origin");
+          }
+          newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+          return new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: newHeaders,
+          });
+        })
+        .catch((e) => console.error(e))
+    );
+  });
+} else {
+  (() => {
+    const reloadedBySelf = window.sessionStorage.getItem("coiReloadedBySelf");
+    window.sessionStorage.removeItem("coiReloadedBySelf");
+    const coepDegrading = (reloadedBySelf == "coepDegrading");
+
+    if (window.crossOriginIsolated || coepDegrading) return;
+
+    const n = navigator;
+    if (n.serviceWorker) {
+        n.serviceWorker.register(window.document.currentScript.src).then(
+        (registration) => {
+            console.log("COI Service Worker registered");
+            registration.addEventListener("updatefound", () => {
+                console.log("Reloading to activate COI Service Worker");
+                window.location.reload();
+            });
+
+            if (registration.active && !n.serviceWorker.controller) {
+                console.log("Reloading to activate COI Service Worker controller");
+                window.location.reload();
+            }
+        },
+        (err) => {
+            console.error("COI Service Worker failed to register", err);
+        }
+        );
+    }
+  })();
 }
